80542dd1d1bdfbeae99568d2d4c2023f63883e2a
ivan <ivan.rudakoff.dev@gmail.com>
2024-05-23 13:32:12 +0300

    test measure temperature

--- main.py
+++ main.py
@@ -1,15 +1,11 @@
 import time
 import board
-import adafruit_dht
+import Adafruit_DHT
 from pyiArduinoI2Ctds import *
 import sys
 
-temperature_array = []
-count = 10
-
-def _start():
+def _tds_start():
     tds = pyiArduinoI2Ctds(None, NO_BEGIN)
-    dht = adafruit_dht.DHT11(board.D18)
     if len(sys.argv) < 2:
         newAddress = 0x09
     else:
@@ -23,45 +19,69 @@ def _start():
                 print("Адрес изменён на " % tds.getAddress())
         else:
                 print("Адрес не изменён!")
-        return [tds,dht]
+        return tds
     else:
         print("Датчик не найден!")
         return None
 
+#start connections
+# DHT
+sensor = Adafruit_DHT.DHT11
+pin = '4'
+
+# TDS
+tds = _tds_start()
+
+
+#local variables
+temperature_array = []
+count = 10
+
 
 def find_average(nums):
     total = 0
-    count = 10
     for num in nums:
         total += num
+    count = len(nums)
     average = total / count
     return average
 
-def measure_temperature(dht):
+def measure_temperature(sensor):
     try:
-        value = dht.temperature
+        humidity,value = Adafruit_DHT.read_retry(sensor, pin)
+        #value = dht.temperature
+        print("measured")
     except:
         value = 20
+    print("current value ",value)
     temperature_array.append(value)
     if len(temperature_array) == count+1:
         temperature_array = temperature_array[1:]
     return sum(temperature_array) / count
 
 def main():
-    check = _start()
-    if not check:
+    if not sensor:
         return
-    tds, dht = check
     while True:
-        temperature = measure_temperature(dht)
-        tds.set_t(temperature) # Текущая температура в градусах цельсия
-        print("Ro="          , end='')
-        print(tds.getRo()    , end='')
-        print("Ом, S="       , end='')
-        print(tds.get_S()    , end='')
-        print("мкСм/см, EC=" , end='')
-        print(tds.getEC()    , end='')
-        print("мкСм/см, TDS=", end='')
-        print(tds.getTDS()   , end='')               #   Выводим количество растворённых твёрдых веществ в жидкости.
-        print(" мг/л\r\n"    , end='')
+        temperature = measure_temperature(sensor)
+        print(temperature)
         time.sleep(1)
+
+#def main():
+#    check = _start()
+#    if not check:
+#        return
+#    tds, dht = check
+#    while True:
+#        temperature = measure_temperature(dht)
+#        tds.set_t(temperature) # Текущая температура в градусах цельсия
+#        print("Ro="          , end='')
+#        print(tds.getRo()    , end='')
+#        print("Ом, S="       , end='')
+#        print(tds.get_S()    , end='')
+#        print("мкСм/см, EC=" , end='')
+#        print(tds.getEC()    , end='')
+#        print("мкСм/см, TDS=", end='')
+#        print(tds.getTDS()   , end='')               #   Выводим количество растворённых твёрдых веществ в жидкости.
+#        print(" мг/л\r\n"    , end='')
+#        time.sleep(1)
